65dd17eb0320b0a4b4b08da53e05ec763916974b
Mikko Knutas <mikko.knutas@myy.haaga-helia.fi>
2017-10-31 12:31:32 +0000

    Moduulit lisätty

---
+++ puppet/modules/ssh/manifests/init.pp
@@ -0,0 +1,17 @@
+class ssh {
+	package {'ssh':
+	ensure => 'installed',
+	allowcdrom => 'true',
+	}
+
+	file {'/etc/ssh/sshd_config':
+	content => template ("ssh/sshd_config"),
+	notify => Service ['ssh'],
+	}
+	
+	service {'ssh':
+		ensure => running,
+		enable => true,
+	}
+}
+ 	
